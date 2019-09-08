aed84f86996896cace590cb854ccee5940ba689f
coocoeman <54761438+coocoeman@users.noreply.github.com>
2019-09-08 12:13:42 +0800

    互動物件

---
+++ EX1/Assets/測試.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 測試 : MonoBehaviour
+{
+    public bool 將江;
+    // Start is called before the first frame update
+    public void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void 試試看()
+    {
+        //if(屬性==XXX||屬性==)
+    }
+}
